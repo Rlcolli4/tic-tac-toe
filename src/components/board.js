8905aa4e55b17afd51288b56b9d834aa8a694575
Robert Collins <robertcollins@Roberts-MacBook-Pro-3.local>
2019-09-30 20:09:38 -0700

    Set up classes in different componenets in a folder

--- src/components/board.js
+++ src/components/board.js
@@ -1,33 +1,44 @@
 import React from 'react';
-import Square from './square';
 
-export class Board extends React.Component {
+function Square(props){
+    return (
+        <button className="square"
+                onClick={() => props.onClick()}>
+            {props.value}
+        </button>
+    );
+}
+
+var Board = class Board extends React.Component {
+    
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square 
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)}
+        />;
     }
 
     render() {
-        const status = 'Next player: X';
-
         return (
         <div>
-            <div className="status">{status}</div>
             <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
             </div>
             <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
             </div>
             <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
             </div>
         </div>
         );
     }
 }
+
+export default Board;
